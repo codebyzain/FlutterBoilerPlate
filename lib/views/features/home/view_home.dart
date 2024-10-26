924f3ab0051cd3fa26c5328cdf362af35cf816bd
zain_royan <107593701+codebyzain@users.noreply.github.com>
2024-10-27 00:42:07 +0700

    Update view_home.dart

--- lib/views/features/home/view_home.dart
+++ lib/views/features/home/view_home.dart
@@ -4,7 +4,6 @@ import 'package:flutter_application_1/utils/util_http_request.dart';
 import 'package:flutter_application_1/utils/util_logger.dart';
 import 'package:flutter_application_1/utils/util_modal.dart';
 import 'package:flutter_application_1/views/shared/button/widget_button.dart';
-import 'package:flutter_application_1/views/shared/text/widget_text.dart';
 import 'package:flutter_application_1/views/shared/text/widget_text_input.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -18,10 +17,8 @@ class ViewHome extends StatelessWidget {
   }
 
   login(context) async {
-    // throw Exception('Another simulated error');
-    // assert(EditableText.debugDeterministicCursor, "wdwd");
     AppLogger.log("Here are the log");
-    await AppNetworkRequest.post("/apdwqdqi/qwdqwd/loginWithPassword",
+    await AppNetworkRequest.post("/",
         data: {
           "username": "123",
           "password": "222",
@@ -39,22 +36,6 @@ class ViewHome extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // ListItem(
-              //   "Change My Password",
-              //   onPress: () => AppModal(context).open(
-              //     child: ListItem(
-              //       "Change My Password",
-              //       onPress: () => AppModal(context)
-              //           .open(child: const ListItem("Change My Password")),
-              //       marginHorizontal: 25,
-              //       description:
-              //           "Apply your new theme, use the Theme.of(context) method when specifying a widget's styling properties. These can include, but are not limited to, style and color.",
-              //     ),
-              //   ),
-              //   marginHorizontal: 25,
-              //   description:
-              //       "Google Fonts makes it easy to bring personality and performance to your websites and products. Our robust catalog of open-source fonts and icons makes it easy to integrate expressive type and icons seamlessly — no matter where you are in the world.",
-              // ),
               const Padding(
                 padding: EdgeInsets.symmetric(
                   horizontal: 25,
@@ -121,9 +102,7 @@ class ViewHome extends StatelessWidget {
                 iconName: Icons.open_in_browser,
                 width: 250,
                 onPress: () {
-                  AppModal(context).open(
-                      child: TextString(
-                          "Device binding is attaching an application instance to a particular mobile device. This method detects a transfer of an application instance to another device. A new install of the application (e.g. in case of buying a new device and transfer the apps) is not detected. The deviceID detects, whether the device identifier has been changed. It is triggered after reinstallation of the application if there are no other applications from the same vendor installed. The value can also change when installing test builds using Xcode or when installing an app on a device using ad-hoc distribution. Below are code snippets demonstrating device binding detection across various platforms Device binding is attaching an application instance to a particular mobile device. This method detects a transfer of an application instance to another device. A new install of the application (e.g. in case of buying a new device and transfer the apps) is not detected. The deviceID detects, whether the device identifier has been changed. It is triggered after reinstallation of the application if there are no other applications from the same vendor installed. The value can also change when installing test builds using Xcode or when installing an app on a device using ad-hoc distribution. Below are code snippets demonstrating device binding detection across various platforms Device binding is attaching an application instance to a particular mobile device. This method detects a transfer of an application instance to another device. A new install of the application (e.g. in case of buying a new device and transfer the apps) is not detected. The deviceID detects, whether the device identifier has been changed. It is triggered after reinstallation of the application if there are no other applications from the same vendor installed. The value can also change when installing test builds using Xcode or when installing an app on a device using ad-hoc distribution. Below are code snippets demonstrating device binding detection across various platforms Device binding is attaching an application instance to a particular mobile device. This method detects a transfer of an application instance to another device. A new install of the application (e.g. in case of buying a new device and transfer the apps) is not detected. The deviceID detects, whether the device identifier has been changed. It is triggered after reinstallation of the application if there are no other applications from the same vendor installed. The value can also change when installing test builds using Xcode or when installing an app on a device using ad-hoc distribution. Below are code snippets demonstrating device binding detection across various platforms"));
+                  AppModal(context).open();
                 },
               ),
             ],
